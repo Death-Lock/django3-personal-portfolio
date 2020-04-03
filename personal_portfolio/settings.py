b17b1124d5f155015360e5422ea90b2ea02048c2
Ishan <ishanagarwal@8e86bfb0e784>
2020-04-03 09:56:31 +0000

    added gitignore

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '045v0&8s(mm#%)-_j@&-(5aa$bbdmr^o^^$=4!z_2-s^+__fc_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ishanagarwal.pythonanywhere.com']
 
 
 # Application definition
@@ -120,5 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
